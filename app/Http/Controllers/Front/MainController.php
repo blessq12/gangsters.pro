af92e2b37f5b3e79c6b48dd306fef9c1932e2b97
Andrew <code70@inbox.ru>
2024-04-04 17:47:01 +0300

    Откат xml теста

--- app/Http/Controllers/Front/MainController.php
+++ app/Http/Controllers/Front/MainController.php
@@ -27,17 +27,4 @@ class MainController extends Controller
     public function contact()
     {
     }
-    public function sendXml()
-    {
-        $client = new \SoapClient('http://smev3-n0.test.gosuslugi.ru:7500/smev/v1.2/ws?wsdl');
-        $response = $client->sendRequest([
-            'SenderProvidedRequestData' => [
-                'MessageID' => '348653456dsf535w',
-                'MessagePrimaryContent' => [
-                    'any' => ''
-                ]
-            ]
-        ]);
-        dd($response);
-    }
 }
