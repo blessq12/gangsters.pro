965874a69a468ce941b58cd06a3d1dd66dfc76a1
Andrew <code70@inbox.ru>
2024-04-28 15:06:40 +0300

    фикс отображения логотипа в навигации и футере

--- resources/views/components/front/navbar.blade.php
+++ resources/views/components/front/navbar.blade.php
@@ -4,7 +4,7 @@
             <div class="col">
                 <a href="{{ route('main.index') }}">
                     <div class="nav-logo">
-                        <img src="{{ $company->logo ? $company->logo->path : 'http://via.placeholder.com/50x50' }}" alt="{{ $company->name }}">
+                        <img src="{{ $company->logo ? $company->logo : 'http://via.placeholder.com/50x50' }}" alt="{{ $company->name }}">
                         <span>
                             {{ $company->name }}
                         </span>
@@ -12,57 +12,57 @@
                 </a>
             </div>
             <div class="col d-none d-lg-block">
-                    <ul class="nav-links" style="justify-content:{{!Request::is('/') ? 'end' : ''}}">
-                        @if (!Request::is('/'))    
-                            <a href="{{ route('main.index') }}">
-                                <li>{{ __('Главная') }}</li>
-                            </a>
-                        @endif
-                        <a href="{{ route('main.about') }}">
-                            <li>{{ __('О компании') }}</li>
-                        </a>
-                        <a href="{{ route('main.vacancy') }}">
-                            <li>{{ __('Вакансии') }}</li>
-                        </a>
-                        <a href="{{ route('main.contact') }}">
-                            <li>{{ __('Контакты') }}</li>
-                        </a>
-                    </ul>
+                <ul class="nav-links" style="justify-content:{{!Request::is('/') ? 'end' : ''}}">
+                    @if (!Request::is('/'))
+                    <a href="{{ route('main.index') }}">
+                        <li>{{ __('Главная') }}</li>
+                    </a>
+                    @endif
+                    <a href="{{ route('main.about') }}">
+                        <li>{{ __('О компании') }}</li>
+                    </a>
+                    <a href="{{ route('main.vacancy') }}">
+                        <li>{{ __('Вакансии') }}</li>
+                    </a>
+                    <a href="{{ route('main.contact') }}">
+                        <li>{{ __('Контакты') }}</li>
+                    </a>
+                </ul>
             </div>
             @if (Request::is('/'))
             <div class="col">
                 <ul class="shop-links">
-                        <li>
-                            <nav-button target="fav"></nav-button>
-                        </li>
-                        <li>
-                            <nav-button target="cart"></nav-button>
-                        </li>
-                        <li class="d-none d-lg-block">
-                            <nav-button target="user"></nav-button>
-                        </li>
-                        <li class="d-block d-lg-none">
-                            <button type="button" class="btn btn-outline-primary" onclick="document.querySelector('.mobile-menu').classList.toggle('show')">
-                                <i class="fa fa-bars"></i>
-                            </button>
-                        </li>
-                    </ul>
-                </div>
+                    <li>
+                        <nav-button target="fav"></nav-button>
+                    </li>
+                    <li>
+                        <nav-button target="cart"></nav-button>
+                    </li>
+                    <li class="d-none d-lg-block">
+                        <nav-button target="user"></nav-button>
+                    </li>
+                    <li class="d-block d-lg-none">
+                        <button type="button" class="btn btn-outline-primary" onclick="document.querySelector('.mobile-menu').classList.toggle('show')">
+                            <i class="fa fa-bars"></i>
+                        </button>
+                    </li>
+                </ul>
+            </div>
             @endif
             @if (!Request::is('/'))
-                <div class="col d-lg-none d-block">
-                    <ul class="shop-links">
-                        <li class="d-block d-lg-none">
-                            <button type="button" class="btn btn-outline-primary" onclick="document.querySelector('.mobile-menu').classList.toggle('show')">
-                                <i class="fa fa-bars"></i>
-                            </button>
-                        </li>
-                    </ul>
-                </div>
+            <div class="col d-lg-none d-block">
+                <ul class="shop-links">
+                    <li class="d-block d-lg-none">
+                        <button type="button" class="btn btn-outline-primary" onclick="document.querySelector('.mobile-menu').classList.toggle('show')">
+                            <i class="fa fa-bars"></i>
+                        </button>
+                    </li>
+                </ul>
+            </div>
             @endif
         </div>
     </div>
     <x-front.mobile-menu>
-        
+
     </x-front.mobile-menu>
 </nav>
