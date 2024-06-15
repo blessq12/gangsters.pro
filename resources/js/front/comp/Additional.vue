15d08d4a0de0a698d5cf0efcb3df07a2e63649ed
Andrew <code70@inbox.ru>
2024-06-15 15:13:10 +0300

    update additional window

--- resources/js/front/comp/Additional.vue
+++ resources/js/front/comp/Additional.vue
@@ -2,13 +2,17 @@
 import { mapStores } from 'pinia';
 import { appStore } from '../../stores/appStorage'
 import { localStore } from '../../stores/localStore';
+
 export default {
     computed: {
-        ...mapStores( appStore, localStore )
+        ...mapStores(appStore, localStore)
     },
     mounted() {
+        // Component is mounted
     },
-    updated(){},
+    updated() {
+        // Component is updated
+    },
     data: () => ({
         prodAdditional: {},
         prodImg: [],
@@ -17,30 +21,27 @@ export default {
     watch: {
         'appStore.additional': {
             handler(val) {
+                document.body.classList.toggle('overflow-hidden', val);
                 if (val) {
-                    document.body.classList.add('overflow-hidden')
                     this.prodAdditional = {
-                    hit: this.appStore.currentAdditional.hit,
-                    spicy: this.appStore.currentAdditional.spicy,
-                    kidsAllow: this.appStore.currentAdditional.kidsAllow,
-                    garlic: this.appStore.currentAdditional.garlic,
-                    onion: this.appStore.currentAdditional.onion,
+                        hit: this.appStore.currentAdditional.hit,
+                        spicy: this.appStore.currentAdditional.spicy,
+                        kidsAllow: this.appStore.currentAdditional.kidsAllow,
+                        garlic: this.appStore.currentAdditional.garlic,
+                        onion: this.appStore.currentAdditional.onion,
+                    }
+                } else {
+                    this.prodAdditional = {}
                 }
-                }
-                else {
-                    document.body.classList.remove('overflow-hidden')
-                    this.prodAdditional =  {}
-                }
-
+                
             }
         },
         imgIndex(val) {
-            if (val > this.appStore.currentAdditional.images.length - 1) {
-                this.imgIndex = 0
-            } 
-            if (val < 0)
-            {
-                this.imgIndex = this.appStore.currentAdditional.images.length - 1
+            const length = this.appStore.currentAdditional.images.length;
+            if (val >= length) {
+                this.imgIndex = 0;
+            } else if (val < 0) {
+                this.imgIndex = length - 1;
             }
         }
     }
@@ -53,44 +54,44 @@ export default {
             enter-active-class="animate__animated animate__fadeIn"
             leave-active-class="animate__animated animate__fadeOut"
         >
-            <div class="wrap" v-if="appStore.additional" >
-                <div class="overlay" @click="appStore.additional=!appStore.additional"></div>
+            <div class="wrap" v-if="appStore.additional">
+                <div class="overlay" @click="appStore.additional = false"></div>
                 <div class="container position-relative" style="z-index: 10;">
-                    
                     <div class="row justify-content-center">
                         <div class="col col-lg-10">
                             <div class="additional-window">
-                                
-                                <div class="row mb-4">
-                                    <div class="col">
-                                        <h4 class="fw-semibold mb-0">{{ appStore.currentAdditional.name }}</h4>
-                                    </div>
-                                    <div class="col text-end">
-                                        <button class="btn-close" @click="appStore.additional = !appStore.additional; appStore.currentAdditional = null"></button>
-                                    </div>
-                                </div>
                                 <div class="row row-cols-1 row-cols-md-2">
                                     <div class="col mb-4 mb-lg-0" v-if="appStore.currentAdditional.images.length">
-                                        <transition-group
-                                            tag="ul"
-                                            class="images list-unstyled"
-                                            enter-active-class="animate__animated animate__fadeIn"
-                                            leave-active-class="animate__animated animate__fadeOut"
-                                        >
-                                            <li 
-                                                class="bg-image rounded"
-                                                style="min-height: 200px;"
-                                                v-for="img in appStore.currentAdditional.thumbs"
-                                                :style="'background: url(' + img.large + ')'"
-                                                :key="img"
-                                                v-show="Array.from(appStore.currentAdditional.thumbs).indexOf( img ) == imgIndex"
+                                        <div class="images-wrap">
+                                            <transition-group
+                                                tag="ul"
+                                                class="images list-unstyled m-0 p-0"
+                                                enter-active-class="animate__animated animate__fadeIn"
+                                                leave-active-class="animate__animated animate__fadeOut"
                                             >
-                                            </li>
-                                        </transition-group>
-                                        <div class="row" v-if="appStore.currentAdditional.images.length > 1">
-                                            <div class="col btn-group">
-                                                <button class="btn btn-outline-primary btn-sm" @click="imgIndex--"><i class="fa fa-arrow-left"></i></button>
-                                                <button class="btn btn-outline-primary btn-sm" @click="imgIndex++"><i class="fa fa-arrow-right"></i></button>
+                                                <li 
+                                                    class="bg-image rounded"
+                                                    style="min-height: 200px;"
+                                                    v-for="img in appStore.currentAdditional.thumbs"
+                                                    :style="'background: url(' + img.large + ')'"
+                                                    :key="img"
+                                                    v-show="Array.from(appStore.currentAdditional.thumbs).indexOf(img) == imgIndex"
+                                                >
+                                                </li>
+                                            </transition-group>
+                                            <div class="nav-group h-100">
+                                                <button 
+                                                    class="h-100"
+                                                    @click="imgIndex -= 1"
+                                                >
+                                                    <i class="fa fa-chevron-left"></i>
+                                                </button>
+                                                <button 
+                                                    class="h-100"
+                                                    @click="imgIndex += 1"
+                                                >
+                                                    <i class="fa fa-chevron-right"></i>
+                                                </button>
                                             </div>
                                         </div>
                                     </div>
@@ -100,6 +101,14 @@ export default {
                                         </ul>
                                     </div>
                                     <div class="col">
+                                        <div class="row justify-content-between mb-2">
+                                            <div class="col-10 d-flex align-items-center">
+                                                <h5 class="mb-0">{{ appStore.currentAdditional.name }}</h5>
+                                            </div>
+                                            <div class="col-2 text-end">
+                                                <button class="btn btn-close" @click="appStore.additional = false"></button>
+                                            </div>
+                                        </div>
                                         <ul class="list-unstyled prod-additionals d-flex align-items-center">
                                             <li v-for="(value, key, index) in prodAdditional" :key="key" style="margin-right: 6px;">
                                                 <div class="disable" v-if="!value" style="background: url('/images/additionals/disabled.png');"></div>
@@ -118,7 +127,7 @@ export default {
                                                 <button 
                                                     class="btn btn-outline-primary rounded-pill"
                                                     :class="{'active' : localStore.checkExist('cart', appStore.currentAdditional)}"
-                                                    @click="localStore.manageStore('cart', appStore.currentAdditional )"
+                                                    @click="localStore.manageStore('cart', appStore.currentAdditional)"
                                                 >
                                                     {{
                                                         localStore.checkExist('cart', appStore.currentAdditional) ?
@@ -134,13 +143,12 @@ export default {
                                                     ></i>
                                                 </button>
                                                 <button class="mx-2 btn btn-outline-primary rounded-pill"
-                                                    @click="localStore.manageStore('fav', appStore.currentAdditional )"
+                                                    @click="localStore.manageStore('fav', appStore.currentAdditional)"
                                                 >В избранное</button>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-
                             </div>
                         </div>
                     </div>
@@ -168,6 +176,29 @@ export default {
             background: $bg-overlay
     .additional-window
         padding: 24px 0
+        .images-wrap
+            position: relative
+            width: 100%
+            .nav-group
+                width: 100%
+                position: absolute
+                top: 50%
+                transform: translateY(-50%)
+                display: flex
+                align-items: center
+                justify-content: space-between
+                button
+                    background: transparent
+                    border: none
+                    color: #fff
+                    width: 40px
+                    cursor: pointer
+                    &:first-child
+                        background: linear-gradient(to right, rgba(255, 255, 255, .5), transparent)
+                        border-radius: 16px 0 0 16px
+                    &:last-child
+                        background: linear-gradient(to left, rgba(255, 255, 255, .5), transparent)
+                        border-radius: 0 16px 16px 0
         @media(min-width: 768px)
             background: #fff
             border-radius: 12px
