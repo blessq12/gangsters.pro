9813f192b93637ca9abb331594790d7133ce7ddd
Andrew Master <code70@inbox.ru>
2026-07-31 11:39:04 +0300

    evolution of home jumbotron

---
+++ public/build/assets/useFavorites-B2Lw3Mzr.js
@@ -0,0 +1,1 @@
+import{f as t}from"./vue-core-Ctk4o703.js";import{o as a}from"./app-B4uj1Vqs.js";function i(){const e=a();return{items:t(()=>e.favorites),count:t(()=>e.count),isFavorite(o){return e.isFavorite(o)}}}function n(){const e=a();return{async toggle(o){await e.toggleFavorite(o)},async remove(o){await e.removeFavorite(o)},async clear(){await e.clear()}}}export{i as a,n as u};
